0c4858ecfe9d57548249d5cc1f9afdb38ba7d48f
Lauren Hazel <lauren.hazel@spsmail.cuny.edu>
2014-09-09 19:39:29 -0400

    assignment 2 changes

---
+++ week 2 assignment.R
@@ -0,0 +1,20 @@
+
+
+queue <- c("James", "Mary", "Steve", "Alex", "Patricia")
+# created a vector with five people in line
+c(queue, "Harold")
+# added "Harold"
+head("James")
+remove(queue, vector = character("James"), pos = -1, envir = as.environment(pos), inherits = FALSE)
+rm("James")
+queue <- x["James" ==remove.value]
+queue <- queue["James" ==remove.value]
+queuu <- queue["James"( queue===remove.value)]
+queue <- queue["James"( queue==remove.value)]
+queue <- ("Mary", "Steve", "Alex", "Patricia", "Harold")
+queue <- c("Mary", "Steve", "Alex", "Patricia" "Harold")
+substr("James")
+queue
+c(queue, "Harold")
+
+length(queue)
