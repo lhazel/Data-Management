2f1a3b2d5a5a4d937755ad3631bc855dce291258
Lauren Hazel <lauren.hazel@spsmail.cuny.edu>
2014-09-12 19:02:43 -0400

    solved quiz 3

--- week 3 quiz.R
+++ week 3 quiz.R
@@ -8,6 +8,28 @@ print(y)
 }
 factorial(12)
 # looop created that calcutlate 12 factorial above
-# using a loop to calculate the final balance of orignal 1500
 
+# creating a vector of 20
 
+x <- c(1,2,3,4,5,6,6,7,8,9,10,11,11,12,13,13,15,16,18,19)
+# calculate sum of every third element in vector created
+
+x[c(3,6,9,12,15,18)]
+
+X + X[C(3,6,9,12,15,18)]
+# creating character vector with same element
+x<- "Stevie"
+X <- rep("Stevie")
+x <- "Stevie"
+rep(x, times=10)
+# element in vector repeated 10
+# create a chr. vector with length 10 repeating 10 times
+x <- "happybirth"
+rep(x,times=10)
+
+# create a vector20 to 50  multipled by 5
+
+x<- 20:50
+
+X*5
+x * 5
