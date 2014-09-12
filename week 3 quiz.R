09dd3f636699ea447afdf4bbe7d6fe58392bf24e
Lauren Hazel <lauren.hazel@spsmail.cuny.edu>
2014-09-12 10:53:28 -0400

    solved first problem in quiz
    solved first problem in quiz

---
+++ week 3 quiz.R
@@ -0,0 +1,13 @@
+factorial <- function(x) {
+  
+  y<-1
+  for (i, n 1:x){
+    y<-y* (1:x)[i])
+print(y)
+  }
+}
+factorial(12)
+# looop created that calcutlate 12 factorial above
+# using a loop to calculate the final balance of orignal 1500
+
+
