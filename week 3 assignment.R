7bcd68cc45d5fd202f89f91f89f994c291a9cf22
Lauren Hazel <lauren.hazel@spsmail.cuny.edu>
2014-09-16 22:35:41 -0400

    assignment 3

---
+++ week 3 assignment.R
@@ -0,0 +1,41 @@
+x <- 1:1000
+while (x % 3 != 0, x % 7 != 0, x % 11 != 0)
+{
+  print(x)
+}
+
+ if (x %3 != 0, x % 7 != 0, x % 11 != 0)
+{
+  print(x)
+}
+
+# Question number 1 above
+
+double.num <- function(x)
+{
+  x * 2
+}
+
+
+double.num(5)
+
+# Question number 2 above
+
+a <- 1:10
+b <- 10:1
+c <- c("pens", "pencils", "glue", "markers", "crayons", 
+       "erasers","paper", "notebooks", "brush", "chalk")
+
+theDF <- data.frame(a, b, c)
+
+theDF
+ return(2:12) 
+
+# Question number 3 above
+
+
+x <- 1:10
+check.bool <- function(x)
+  
+
+
